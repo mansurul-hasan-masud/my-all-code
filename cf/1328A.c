2bdb00cf84ab13d6b0df1b011d8eae3a50ba82bd
Mansurul_Hasan_Masud <mhmasud006@gmail.com>
2020-04-29 16:01:06 +0600

    cf

---
+++ cf/1328A.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		int a, b;
+		scanf("%d %d",&a, &b);
+		if(a % b == 0)
+			printf("%d\n",0);
+		else
+			printf("%d\n",b - a % b);
+	}
+}
