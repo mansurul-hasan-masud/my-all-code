ac0b56611259541342909dbee36f3e08882c890b
Mansurul_Hasan_Masud <mhmasud006@gmail.com>
2020-05-09 13:05:28 +0600

    cf71A

---
+++ cf/71A.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	char s[1000];
+	for(int i = 0; i < t; i++) {
+		scanf("%s", s);
+		int l = strlen(s);
+		if(l > 10) {
+			printf("%c%d%c\n",s[0], l - 2, s[l - 1]);			
+		} else {
+			printf("%s\n", s);
+		}
+	}
+	
+	
+	return 0;
+}
