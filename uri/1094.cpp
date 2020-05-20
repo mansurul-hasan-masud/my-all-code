c275a5dcd7739120f6819df4dd118c2c6d80561f
Mansurul_Hasan_Masud <mhmasud006@gmail.com>
2020-05-20 15:13:59 +0600

    uri

---
+++ uri/1094.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, t, n, total, l, C = 0, R = 0, S = 0;
+	char c;
+	double x, y, z;
+	scanf("%d", &n);
+	
+	for(i = 0; i < n; i++) {
+		scanf("%d %c", &l, &c);
+		
+		if(c == 'S')S += l;
+		else if(c == 'R') R += l;
+		else C += l;
+	}
+	
+	total = C + S + R;
+	x = (C*100.00)/total;
+	y = (R*100.00)/total;
+	z = (S*100.00)/total;
+
+	printf("Total: %d cobaias\n", total);
+	printf("Total de coelhos: %d\n", C);
+	printf("Total de ratos: %d\n", R);
+	printf("Total de sapos: %d\n", S);
+	printf("Percentual de coelhos: %0.2lf %%\n", x);
+	printf("Percentual de ratos: %0.2lf %%\n", y);
+	printf("Percentual de sapos: %0.2lf %%\n", z);
+
+	
+	return 0;
+}
---
+++ uri/1116.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int t;
+	float a, b;
+	scanf("%d", &t);
+	
+	for(int i = 0; i < t; i++) {
+		scanf("%f %f", &a, &b);
+		if(b == 0) printf("divisao impossivel\n");
+		else printf("%0.1f\n", a / b);
+	}
+	
+	return 0;
+}
